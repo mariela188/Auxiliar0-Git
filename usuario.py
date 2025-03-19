363790809f63332ee944637865411016e41992e8
Mariela Cornejo <mariela.cornejo@ug.uchile.cl>
2025-03-19 19:22:45 -0300

    persona A

--- usuario.py
+++ usuario.py
@@ -11,4 +11,5 @@ class Usuario:
     def listarTareas(self):
         for tarea in self.tareas:
             if tarea.estaLista():
-                print(f"[X] {tarea.obtenerNombre()}")
+                print(f"[X] {tarea.obtenerNombre()} está lista")
+                print(f"[ ] {tarea.obtenerNombre()} no está lista")  
